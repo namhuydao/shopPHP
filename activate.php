31ac325e03724c0fc389c152c1df2f0bd294b498
namhuydao <namhuydao@tutamail.com>
2021-01-18 14:00:55 +0700

    add files

---
+++ activate.php
@@ -0,0 +1,11 @@
+<?php include './database.php' ?>
+<?php include './function.php' ?>
+<?php
+$id = $_GET['id'];
+$sql = "UPDATE  product SET status = '1' WHERE id ='$id'";
+if ($conn->query($sql) === TRUE) {
+    echo "<script>alert('Product Activated')</script>";
+    header("location:index.php");
+} else {
+    echo '("Product not activated")';
+}
